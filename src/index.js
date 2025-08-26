bad9c7d3102ffc32a9bf35399ee6442e69b25edc
RosaBen <benchabane.rosa@outlook.com>
2025-08-27 01:21:08 +0200

    created urls for different informations

--- src/index.js
+++ src/index.js
@@ -1,31 +1,61 @@
 import './style/index.scss';
 
-const url = process.env.API_URL;
+const key = process.env.API_KEY;
+const apiUrl = "https://api.rawg.io/api/";
+const keyParam = "?key=";
+
+const apiDatasParams = [
+  "games",
+  "creators",
+  "developers",
+  "genres",
+  "platforms",
+  "publishers",
+  "tags"
+];
+
+
+
+const createUrls = () => {
+  return apiDatasParams.reduce((acc, param) => {
+    acc[param] = `${apiUrl}${param}${keyParam}${key}`;
+    return acc;
+  }, {});
+
+}
+
+const urls = createUrls();
+console.log("urls", urls);
 // page = 1, pageSize = 40, filters = null
-const getApi = async (page = 1, pageSize = 9) => {
-  try {
-    // let apiUrl = `${url}&page=3`;
-    let apiUrl = `${url}&page_size=${pageSize}&page=${page}`;
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log("data", data)
-    console.log("results", data.results)
-    return {
-      results: data.results
-    }
+// const getApi = async (page = 1, pageSize = 9) => {
+//   try {
+//     // let apiUrl = `${url}&page=3`;
+//     let gamesUrl = `${url}&page_size=${pageSize}&page=${page}`;
+//     const response = await fetch(gamesUrl);
+//     const data = await response.json();
+//     console.log("data", data.results)
+//     return {
+//       results: data.results,
+//       next: data.next
+//     }
+
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//     return {
+//       results: [], next: null
+//     };
+//   }
+// }
+
+// const getDatas = () => {
+//   const data = async getDatas();
+//   console.log("data", data.results)
+// }
 
 
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return {
-      results: [], next: null
-    };
-  }
-}
+// const initPage = () => {
+//   getApi();
 
+// }
 
-const initPage = () => {
-  getApi();
-}
-
-initPage();
+// initPage();
