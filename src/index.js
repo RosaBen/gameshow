73c4e91c31d4a63c55e909ca05b8a456f5d451be
RosaBen <benchabane.rosa@outlook.com>
2025-08-26 19:20:29 +0200

    get api succesfully

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-import './style/index.scss'; 
+import './style/index.scss';
+
+const url = process.env.API_URL;
+// page = 1, pageSize = 40, filters = null
+const getApi = async (page = 1, pageSize = 9) => {
+  try {
+    // let apiUrl = `${url}&page=3`;
+    let apiUrl = `${url}&page_size=${pageSize}&page=${page}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log("data", data)
+    console.log("results", data.results)
+    return {
+      results: data.results
+    }
+
+
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return {
+      results: [], next: null
+    };
+  }
+}
+
+
+const initPage = () => {
+  getApi();
+}
+
+initPage();
