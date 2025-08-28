2fee1954b8dfcba5e46ed41b826d77c634959690
RosaBen <benchabane.rosa@outlook.com>
2025-08-28 23:06:08 +0200

    successfully got paramsfrom api

--- src/index.js
+++ src/index.js
@@ -48,54 +48,56 @@ document.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('header');
   const main = document.querySelector('main');
   const footer = document.querySelector('footer');
-  const platforms = ['Any', 'PC', 'Xbox'];
-  const cards = [{
-    poster: "https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg",
-    title: 'Gametitle',
-    platforms: ['p1', 'p2', 'p3'],
-    released: 'Released: 25 sep 2025',
-    editor: 'Editor: jgfskgsaashasfh',
-    rating: 4.45,
-    count: '1200 votes',
-    genres: ['g1', 'g2', 'g3', 'g4', 'g5', 'g6']
-  },
-  {
-    poster: "https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg",
-    title: 'Gametitle',
-    platforms: ['p1', 'p2', 'p3'],
-    released: 'Released: 25 sep 2025',
-    editor: 'Editor: jgfskgsaashasfh',
-    rating: 4.45,
-    count: '1200 votes',
-    genres: ['g1', 'g2', 'g3', 'g4', 'g5', 'g6']
-  },
-  {
-    poster: "https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg",
-    title: 'Gametitle',
-    platforms: ['p1', 'p2', 'p3'],
-    released: 'Released: 25 sep 2025',
-    editor: 'Editor: jgfskgsaashasfh',
-    rating: 4.45,
-    count: '1200 votes',
-    genres: ['g1', 'g2', 'g3', 'g4', 'g5', 'g6']
-  },
-  {
-    poster: "https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg",
-    title: 'Gametitle',
-    platforms: ['p1', 'p2', 'p3'],
-    released: 'Released: 25 sep 2025',
-    editor: 'Editor: jgfskgsaashasfh',
-    rating: 4.45,
-    count: '1200 votes',
-    genres: ['g1', 'g2', 'g3', 'g4', 'g5', 'g6']
-  }
+  // const platforms = ['Any', 'PC', 'Xbox'];
+  // const cards = [{
+  //   poster: "https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg",
+  //   title: 'Gametitle',
+  //   platforms: ['p1', 'p2', 'p3'],
+  //   released: 'Released: 25 sep 2025',
+  //   editor: 'Editor: jgfskgsaashasfh',
+  //   rating: 4.45,
+  //   count: '1200 votes',
+  //   genres: ['g1', 'g2', 'g3', 'g4', 'g5', 'g6']
+  // },
+  // {
+  //   poster: "https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg",
+  //   title: 'Gametitle',
+  //   platforms: ['p1', 'p2', 'p3'],
+  //   released: 'Released: 25 sep 2025',
+  //   editor: 'Editor: jgfskgsaashasfh',
+  //   rating: 4.45,
+  //   count: '1200 votes',
+  //   genres: ['g1', 'g2', 'g3', 'g4', 'g5', 'g6']
+  // },
+  // {
+  //   poster: "https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg",
+  //   title: 'Gametitle',
+  //   platforms: ['p1', 'p2', 'p3'],
+  //   released: 'Released: 25 sep 2025',
+  //   editor: 'Editor: jgfskgsaashasfh',
+  //   rating: 4.45,
+  //   count: '1200 votes',
+  //   genres: ['g1', 'g2', 'g3', 'g4', 'g5', 'g6']
+  // },
+  // {
+  //   poster: "https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg",
+  //   title: 'Gametitle',
+  //   platforms: ['p1', 'p2', 'p3'],
+  //   released: 'Released: 25 sep 2025',
+  //   editor: 'Editor: jgfskgsaashasfh',
+  //   rating: 4.45,
+  //   count: '1200 votes',
+  //   genres: ['g1', 'g2', 'g3', 'g4', 'g5', 'g6']
+  // }
 
-  ]
+  // ]
   // const urls = createUrls();
   // const { results, next } = await getGames(urls);
 
   // get params from api
-  const getParamsGames = (results) => {
+  const getParamsGames = async () => {
+    const urls = createUrls();
+    const { results } = await getGames(urls);
     return results.map(result => ({
       slug: result.slug,
       title: result.name,
@@ -105,13 +107,14 @@ document.addEventListener('DOMContentLoaded', () => {
       platforms: result.platforms,
       rating: result.rating,
       votes: result.ratings_count,
-      editors: result.publishers,
+      editor: result.publishers,
       gameId: result.id
     }));
   }
 
   // Create body of html to share per pages
   const htmlBody = () => {
+
     // navbar--------------------------------------
     const nav = document.createElement('nav');
     const navDiv1 = document.createElement('div');
@@ -159,148 +162,144 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // create Html for homepage
-  const createHtmlHomepage = () => {
-    const bodyHtml = htmlBody();
-    // Header-------------------------------------------------------------
-    const nextChildHeader = document.querySelector('.animated-border');
-    const divHomeText = document.createElement('div');
-    header.insertBefore(divHomeText, nextChildHeader);
-    divHomeText.classList.add('home-header');
-    const homeHeaderH2 = document.createElement('h2');
-    const homeHeaderP = document.createElement('p');
-    homeHeaderH2.textContent = 'Welcome,';
-    homeHeaderP.textContent = 'The Hyper Progame is the world’s premier event for computer and video games and related products. At The Hyper Progame,the video game industry’s top talent pack the Los Angeles Convention Center, connecting tens of thousands of the best,brightest, and most innovative in the interactive entertainment industry.For three exciting days, leading - edge companies, groundbreaking new technologies, and never - before - seen products will be showcased.The Hyper Progame connects you with both new and existing partners, industry executives, gamers, and social influencers providing unprecedented exposure.';
-    divHomeText.appendChild(homeHeaderH2);
-    divHomeText.appendChild(homeHeaderP);
+  const createHtmlHomepage = async () => {
+    try {
+      const gamesData = await getParamsGames();
+
+      htmlBody();
+
+      // Header-------------------------------------------------------------
+      const nextChildHeader = document.querySelector('.animated-border');
+      const divHomeText = document.createElement('div');
+      header.insertBefore(divHomeText, nextChildHeader);
+      divHomeText.classList.add('home-header');
+      const homeHeaderH2 = document.createElement('h2');
+      const homeHeaderP = document.createElement('p');
+      homeHeaderH2.textContent = 'Welcome,';
+      homeHeaderP.textContent = 'The Hyper Progame is the world’s premier event for computer and video games and related products. At The Hyper Progame, the video game industry’s top talent pack the Los Angeles Convention Center, connecting tens of thousands of the best, brightest, and most innovative in the interactive entertainment industry. For three exciting days, leading-edge companies, groundbreaking new technologies, and never-before-seen products will be showcased. The Hyper Progame connects you with both new and existing partners, industry executives, gamers, and social influencers providing unprecedented exposure.';
+      divHomeText.appendChild(homeHeaderH2);
+      divHomeText.appendChild(homeHeaderP);
 
 
-    // Main
-    // platforms Filter
-    const divFilterHome = document.createElement('div');
-    divFilterHome.classList.add('home-selection');
-    const homePlatformH2 = document.createElement('h2');
-    homePlatformH2.textContent = 'Platforms: ';
-    const homePlatformFilter = document.createElement('select');
-    homePlatformFilter.name = 'home-platforms';
-    homePlatformFilter.id = 'home-platforms';
-    platforms.forEach((platform, index) => {
-      const option = document.createElement('option');
-      option.value = `option${index + 1}`;
-      option.textContent = platform;
-      homePlatformFilter.appendChild(option);
-    })
-    divFilterHome.appendChild(homePlatformH2);
-    divFilterHome.appendChild(homePlatformFilter);
+      // Main
+      // Platforms Filter
+      const divFilterHome = document.createElement('div');
+      divFilterHome.classList.add('home-selection');
+      const homePlatformH2 = document.createElement('h2');
+      homePlatformH2.textContent = 'Platforms: ';
+      const homePlatformFilter = document.createElement('select');
+      homePlatformFilter.name = 'home-platforms';
+      homePlatformFilter.id = 'home-platforms';
 
-    // Section 
-    const homeSection = document.createElement('section');
-    // cards
-    const divCardsContainer = document.createElement('div');
-    divCardsContainer.classList.add('cards-container');
-    // A card
-    cards.forEach(card => {
-      const divCardHome = document.createElement('div');
-      divCardHome.classList.add('home-card');
-      // front card
-      const cardFront = document.createElement('div');
-      cardFront.classList.add('front-card');
-      const frontImgDiv = document.createElement('div');
-      frontImgDiv.classList.add('front-div-img');
-      const cardPoster = document.createElement('img');
-      cardPoster.src = card.poster;
-      cardPoster.alt = card.title;
-      cardPoster.classList.add('home-poster');
-      const frontdetailsDiv = document.createElement('div');
-      frontdetailsDiv.classList.add('front-details');
-      const cardTitle = document.createElement('h3');
-      cardTitle.textContent = card.title;
-      const cardPlatforms = document.createElement('div');
-      cardPlatforms.classList.add('home-platforms-container');
-      const platformsH4 = document.createElement('h4');
-      platformsH4.textContent = 'Platforms';
-      const platformsList = document.createElement('ul');
-      card.platforms.forEach(platform => {
-        const li = document.createElement('li');
-        li.classList.add('fr-platform-item');
-        li.textContent = platform;
-        platformsList.appendChild(li);
+      const defaultOption = document.createElement('option');
+      defaultOption.value = 'Default';
+      defaultOption.textContent = 'Any';
+      homePlatformFilter.appendChild(defaultOption);
+      gamesData.forEach((game, index) => {
+        if (game.platforms) {
+          game.platforms.forEach((platform, platformIndex) => {
+            const option = document.createElement('option');
+            option.value = `option${index + 1}-${platformIndex + 1}`;
+            option.textContent = platform.platform.name;
+            homePlatformFilter.appendChild(option);
+          });
+        }
+      });
 
-      })
+      divFilterHome.appendChild(homePlatformH2);
+      divFilterHome.appendChild(homePlatformFilter);
 
-      // back card
+      // Section 
+      const homeSection = document.createElement('section');
+      // Cards
+      const divCardsContainer = document.createElement('div');
+      divCardsContainer.classList.add('cards-container');
 
-      const cardBack = document.createElement('div');
-      cardBack.classList.add('back-card');
-      const divBackDetails = document.createElement('div');
-      divBackDetails.classList.add('card-back-details');
-      const divPText = document.createElement('div');
-      divPText.classList.add('back-p-text');
-      const pReleased = document.createElement('p');
-      const pEditor = document.createElement('p');
-      pReleased.textContent = card.released;
-      pEditor.textContent = card.editor;
-      const divRateCount = document.createElement('div');
-      divRateCount.classList.add('back-p-rate-vote');
-      const pRating = document.createElement('p');
-      const pCount = document.createElement('p');
-      pRating.textContent = card.rating;
-      pCount.textContent = card.count;
-      const divBackGenres = document.createElement('div');
-      divBackGenres.classList.add('home-genres-container');
-      const genresH4 = document.createElement('h4');
-      genresH4.textContent = 'Genres';
-      const genresList = document.createElement('ul');
-      (card.genres).forEach(genre => {
-        const li = document.createElement('li');
-        li.classList.add('bc-genre-item');
-        li.textContent = genre;
-        genresList.appendChild(li);
-      })
+      gamesData.forEach(card => {
+        const divCardHome = document.createElement('div');
+        divCardHome.classList.add('home-card');
+        // Front card
+        const cardFront = document.createElement('div');
+        cardFront.classList.add('front-card');
+        const frontImgDiv = document.createElement('div');
+        frontImgDiv.classList.add('front-div-img');
+        const cardPoster = document.createElement('img');
+        cardPoster.src = card.poster;
+        cardPoster.alt = card.title;
+        cardPoster.classList.add('home-poster');
+        const frontdetailsDiv = document.createElement('div');
+        frontdetailsDiv.classList.add('front-details');
+        const cardTitle = document.createElement('h3');
+        cardTitle.textContent = card.title;
+        const cardPlatforms = document.createElement('div');
+        cardPlatforms.classList.add('home-platforms-container');
+        const platformsH4 = document.createElement('h4');
+        platformsH4.textContent = 'Platforms';
+        const platformsList = document.createElement('ul');
+        card.platforms.forEach(platform => {
+          const li = document.createElement('li');
+          li.classList.add('fr-platform-item');
+          li.textContent = platform.platform.name;
+          platformsList.appendChild(li);
+        });
 
-      divBackGenres.appendChild(genresH4);
-      divBackGenres.appendChild(genresList);
-      divRateCount.appendChild(pRating);
-      divRateCount.appendChild(pCount);
-      divPText.appendChild(pReleased);
-      divPText.appendChild(pEditor);
-      divBackDetails.appendChild(divPText);
-      divBackDetails.appendChild(divRateCount);
-      cardPlatforms.appendChild(platformsH4);
-      cardPlatforms.appendChild(platformsList);
-      frontdetailsDiv.appendChild(cardTitle);
-      frontdetailsDiv.appendChild(cardPlatforms);
-      frontImgDiv.appendChild(cardPoster);
-      cardFront.appendChild(frontImgDiv);
-      cardFront.appendChild(frontdetailsDiv);
-      cardBack.appendChild(divBackDetails);
-      cardBack.appendChild(divBackGenres);
-      divCardHome.appendChild(cardFront);
-      divCardHome.appendChild(cardBack);
-      divCardsContainer.appendChild(divCardHome);
+        // Back card
+        const cardBack = document.createElement('div');
+        cardBack.classList.add('back-card');
+        const divBackDetails = document.createElement('div');
+        divBackDetails.classList.add('card-back-details');
+        const divPText = document.createElement('div');
+        divPText.classList.add('back-p-text');
+        const pReleased = document.createElement('p');
+        const pEditor = document.createElement('p');
+        pReleased.textContent = card.released;
+        pEditor.textContent = card.editor;
+        const divRateCount = document.createElement('div');
+        divRateCount.classList.add('back-p-rate-vote');
+        const pRating = document.createElement('p');
+        const pCount = document.createElement('p');
+        pRating.textContent = `Rating: ${card.rating}`;
+        pCount.textContent = `Votes: ${card.votes}`;
+        const divBackGenres = document.createElement('div');
+        divBackGenres.classList.add('home-genres-container');
+        const genresH4 = document.createElement('h4');
+        genresH4.textContent = 'Genres';
+        const genresList = document.createElement('ul');
+        card.genres.forEach(genre => {
+          const li = document.createElement('li');
+          li.classList.add('bc-genre-item');
+          li.textContent = genre.name;
+          genresList.appendChild(li);
+        });
 
+        divBackGenres.appendChild(genresH4);
+        divBackGenres.appendChild(genresList);
+        divRateCount.appendChild(pRating);
+        divRateCount.appendChild(pCount);
+        divPText.appendChild(pReleased);
+        divPText.appendChild(pEditor);
+        divBackDetails.appendChild(divPText);
+        divBackDetails.appendChild(divRateCount);
+        cardPlatforms.appendChild(platformsH4);
+        cardPlatforms.appendChild(platformsList);
+        frontdetailsDiv.appendChild(cardTitle);
+        frontdetailsDiv.appendChild(cardPlatforms);
+        frontImgDiv.appendChild(cardPoster);
+        cardFront.appendChild(frontImgDiv);
+        cardFront.appendChild(frontdetailsDiv);
+        cardBack.appendChild(divBackDetails);
+        cardBack.appendChild(divBackGenres);
+        divCardHome.appendChild(cardFront);
+        divCardHome.appendChild(cardBack);
+        divCardsContainer.appendChild(divCardHome);
+      });
 
-
-    })
-
-    // button
-    // const divShowMoreBtn = document.createElement('div');
-    // divShowMoreBtn.classList.add('showMoreDiv');
-    // const showMoreBtn = document.createElement('button');
-    // showMoreBtn.type = 'button';
-    // showMoreBtn.classList.add('showMoreBtn');
-    // showMoreBtn.textContent = 'Show More';
-    // divShowMoreBtn.appendChild(showMoreBtn);
-
-
-
-    homeSection.appendChild(divCardsContainer);
-    // homeSection.appendChild(divShowMoreBtn);
-    main.appendChild(divFilterHome);
-    main.appendChild(homeSection);
-    return {
-      bodyHtml, divFilterHome, homeSection
+      homeSection.appendChild(divCardsContainer);
+      main.appendChild(divFilterHome);
+      main.appendChild(homeSection);
+    } catch (error) {
+      console.error('Error in createHtmlHomepage:', error);
     }
-    // , divHomeText, homeHeaderH2, homeHeaderP, homePlatformH2, homePlatformFilter, homecard, homeSection, divCardsContainer, divShowMoreBtn
   }
 
 
@@ -318,13 +317,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   // Initialize page
-  const initPage = async () => {
-    const urls = createUrls();
-    const { results } = await getGames(urls);
-    getParamsGames(results);
+  const initPage = () => {
     createHtmlHomepage();
-    searchPlaceholder();
-    console.log('results:', getParamsGames(results));
+    // searchPlaceholder();
+
   };
 
   initPage();
